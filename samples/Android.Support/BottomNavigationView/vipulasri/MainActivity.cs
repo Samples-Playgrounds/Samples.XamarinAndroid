50b7148fc7d51f7e339832c3a6621a1d993103e4
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-01-04 16:34:00 +0100

    vipulasri sample

---
+++ samples/Android.Support/BottomNavigationView/vipulasri/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace vipulasri
+{
+    [Activity(Label = "vipulasri", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : Android.Support.V7.App.AppCompatActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.Design.Widget.BottomNavigationView bottomNavigationView = null;
+            bottomNavigationView = FindViewById<Android.Support.Design.Widget.BottomNavigationView>(Resource.Id.bottom_navigation);
+
+            int icon_size = bottomNavigationView.ItemIconSize;
+
+            return;
+        }
+    }
+}
+
