fa67bc880d5576caa118e3fce424acf97af3fe2a
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-01-04 14:54:13 +0100

    NealRDC sample

---
+++ samples/Android.Support/BottomNavigationView/NealRDC/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace NealRDC
+{
+    [Activity(Label = "NealRDC", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : Android.Support.V7.App.AppCompatActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.Design.Widget.BottomNavigationView bottomNavigationView = null;
+            bottomNavigationView = FindViewById<Android.Support.Design.Widget.BottomNavigationView>(Resource.Id.navigation);
+
+        }
+    }
+}
+
