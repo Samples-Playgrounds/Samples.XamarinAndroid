db43110b7564e6e1f1d9c5e9440ddf1883bc635b
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-01-11 19:03:10 +0100

    sample complete w Resource issues

--- samples/Android.Support/BottomNavigationView/hitherejoe/MainActivity.cs
+++ samples/Android.Support/BottomNavigationView/hitherejoe/MainActivity.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-
+    
 namespace hitherejoe
 {
     [Activity(Label = "hitherejoe", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
@@ -23,6 +23,34 @@ namespace hitherejoe
             bottomNavigationView = FindViewById<Android.Support.Design.Widget.BottomNavigationView>(Resource.Id.bottom_navigation);
 
             int icon_size = bottomNavigationView.ItemIconSize;
+
+            textFavorites = FindViewById<TextView>(R.id.text_favorites);
+            textSchedules = FindViewById<TextView>(R.id.text_schedules);
+            textMusic = FindViewById<TextView>(R.id.text_music);
+
+            bottomNavigationView.NavigationItemSelected += (sender, e) =>
+            {
+                //switch (e.Item.ItemId)
+                //{
+                //case Resources.Id.action_favorites:
+                //    textFavorites.Visibility = Android.Views.ViewStates.Visible;
+                //    textSchedules.Visibility = Android.Views.ViewStates.Gone;
+                //    textMusic.Visibility = Android.Views.ViewStates.Gone;
+                //    break;
+                //case Resources.Id.action_schedules:
+                //    textFavorites.SetVisibility(Android.Views.ViewStates.Gone);
+                //    textSchedules.SetVisibility(Android.Views.ViewStates.Visible);
+                //    textMusic.SetVisibility(Android.Views.ViewStates.Gone);
+                //    break;
+                //case R.id.action_music:
+                //textFavorites.setVisibility(Android.Views.ViewStates.Gone);
+                //textSchedules.setVisibility(Android.Views.ViewStates.Gone);
+                //textMusic.setVisibility(Android.Views.ViewStates.Visible);
+                //break;
+                //}
+
+                return;
+            };
 
             return;
         }
