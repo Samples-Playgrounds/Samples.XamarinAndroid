2db4fa39f44e1932b60863d82c8c7b8eb58efd75
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-04-08 18:27:20 +0200

    Firebase.Perf github issue NewTrace sample

---
+++ samples/GooglePlayServices-Firebase/README.md
@@ -0,0 +1,12 @@
+# Xamarin.Android.Samples
+
+
+*   Firebase.Perf sample
+
+    *   https://firebase.google.com/docs/perf-mon/get-started-android#optionaldefine-a-custom-trace-and-one-or-more-metrics-in-your-app
+    
+    *   https://github.com/jldubz/Xamarin-Android-Firebase-Performance-Test
+
+    *   github Firebase issues:
+
+        *   https://github.com/xamarin/GooglePlayServicesComponents/issues/179
---
+++ samples/GooglePlayServices-Firebase/Xamarin-Android-Firebase-Performance-Test-master/FirebasePerfTest/FirebasePerfTest.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Widget;
+using System;
+
+namespace FirebasePerfTest.Droid
+{
+    [Activity(Label = "FirebasePerfTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var button = FindViewById<Button>(Resource.Id.button_execute);
+            button.Click += (send, args) => {
+
+                var uri = new Uri("https://www.google.com/");
+                NetworkHelper.GetRequestAsync(uri).ContinueWith(task => {
+                    if (task.IsFaulted) {
+                        button.Post(() => {
+                            Toast.MakeText(this, "Call failed", ToastLength.Long).Show();
+                        });
+                        
+                        return;
+                    }
+                    button.Post(() => {
+                        Toast.MakeText(this, "Call succeeded", ToastLength.Long).Show();
+                    });
+                });
+            };
+
+            Firebase.FirebaseApp fbapp = null;
+            Firebase.Perf.FirebasePerformance fbp = new Firebase.Perf.FirebasePerformance(fbapp);
+
+            Firebase.Perf.Metrics.Trace t1 = Firebase.Perf.FirebasePerformance.Instance.NewTrace("t1");
+            Firebase.Perf.Metrics.Trace t2 = fbp.NewTrace("t2");
+
+            return;
+        }
+    }
+}
